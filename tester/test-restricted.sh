1da9f11c1b59fd1f886ef03f3423ab3f27039989
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-03-19 21:23:25 +0100

    rename to metro-package-exports

--- tester/test-restricted.sh
+++ tester/test-restricted.sh
@@ -1,15 +1,15 @@
 echo 'From Node.js:'
 echo
-node -e "console.log(require.resolve('@metro-exports/restricted/features/internal/b.js'))"
+node -e "console.log(require.resolve('@metro-package-exports/restricted/features/internal/b.js'))"
 
 echo
 echo '----------------------------------------'
 echo 'From enhanced-resolve:'
 echo
-node enhanced-resolve.js "@metro-exports/restricted/features/internal/b.js"
+node enhanced-resolve.js "@metro-package-exports/restricted/features/internal/b.js"
 
 echo
 echo '----------------------------------------'
 echo 'From metro-resolve:'
 echo
-node metro-resolver.js "@metro-exports/restricted/features/internal/b.js"
+node metro-resolver.js "@metro-package-exports/restricted/features/internal/b.js"
--- tester/test-root-shorthand.sh
+++ tester/test-root-shorthand.sh
@@ -1,15 +1,15 @@
 echo 'From Node.js:'
 echo
-node -e "console.log(require.resolve('@metro-exports/root-shorthand'))"
+node -e "console.log(require.resolve('@metro-package-exports/root-shorthand'))"
 
 echo
 echo '----------------------------------------'
 echo 'From enhanced-resolve:'
 echo
-node enhanced-resolve.js "@metro-exports/root-shorthand"
+node enhanced-resolve.js "@metro-package-exports/root-shorthand"
 
 echo
 echo '----------------------------------------'
 echo 'From metro-resolve:'
 echo
-node metro-resolver.js "@metro-exports/root-shorthand"
+node metro-resolver.js "@metro-package-exports/root-shorthand"
