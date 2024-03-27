2f5562cbce75eacf50005c1d590520027ee4127b
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2024-03-27 14:59:51 +0100

    use variable for resolution target in scripts

--- tester/test-restricted.sh
+++ tester/test-restricted.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+TARGET_PATH="@metro-package-exports/restricted/features/internal/b.js"
+
+echo "Resolution target: $TARGET_PATH"
+echo
 
 echo 'From Node.js:'
 echo
-node -e "console.log(require.resolve('@metro-package-exports/restricted/features/internal/b.js'))"
+node -e "console.log(require.resolve('$TARGET_PATH'))"
 
 echo
 echo '----------------------------------------'
 echo 'From enhanced-resolve:'
 echo
-node enhanced-resolve.js "@metro-package-exports/restricted/features/internal/b.js"
+node enhanced-resolve.js "$TARGET_PATH"
 
 echo
 echo '----------------------------------------'
 echo 'From metro-resolve:'
 echo
-node metro-resolver.js "@metro-package-exports/restricted/features/internal/b.js"
+node metro-resolver.js "$TARGET_PATH"
--- tester/test-root-shorthand.sh
+++ tester/test-root-shorthand.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+TARGET_PATH="@metro-package-exports/root-shorthand"
+
+echo "Resolution target: $TARGET_PATH"
+echo
 
 echo 'From Node.js:'
 echo
-node -e "console.log(require.resolve('@metro-package-exports/root-shorthand'))"
+node -e "console.log(require.resolve('$TARGET_PATH'))"
 
 echo
 echo '----------------------------------------'
 echo 'From enhanced-resolve:'
 echo
-node enhanced-resolve.js "@metro-package-exports/root-shorthand"
+node enhanced-resolve.js "$TARGET_PATH"
 
 echo
 echo '----------------------------------------'
 echo 'From metro-resolve:'
 echo
-node metro-resolver.js "@metro-package-exports/root-shorthand"
+node metro-resolver.js "$TARGET_PATH"
